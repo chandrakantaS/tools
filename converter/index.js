6f2b7d8c8efe7c238a79d0318dc6478b44fea064
chandrakanta <ckoo7shatpathy@gmail.com>
2017-12-11 20:59:40 +0530

    Add coments

--- converter/index.js
+++ converter/index.js
@@ -10,57 +10,63 @@ const command = args[2]
 
 
 const options = {};
+// convert the console options to n object
 args.filter(arg => {
    return /-/.test(arg)
 }).forEach(opt => {
    options[opt.replace(/-/g, '')] = true;
 })
 
-console.log(options);
 
-function encrypt (data) {
+function encrypt (data) { //function to encrypt a file
    const cipher = crypto.createCipher('aes-256-ctr', 'a secret key');
-   if (Buffer.isBuffer(data)) {
+   if (Buffer.isBuffer(data)) { //handle separately if its buffer
       const crypted = Buffer.concat([cipher.update(data),cipher.final()]);
       return crypted;
+   } else {
+      // handle if not buffer
    }
+
 }
 
 function decrypt(data) {
    const decipher = crypto.createDecipher('aes-256-ctr', 'a secret key');
-   var dec = Buffer.concat([decipher.update(data) , decipher.final()]);
-   return dec;
+   if (Buffer.isBuffer(data)) {
+      var dec = Buffer.concat([decipher.update(data) , decipher.final()]);
+      return dec;
+   } else {
+         // handle if data is not buffer.
+   }
+
 }
 
 
-function readAndWrite(i, o, enc) {
-   console.log(i, o)
-   let readStream = fs.createReadStream(i);
-   let writeSream = fs.createWriteStream(o);
+function readAndWrite(i, o, enc) { // copy file from source and pastes after enc/dec
+   let readStream = fs.createReadStream(i); //creates a read stream from input path
+   let writeSream = fs.createWriteStream(o);// creates write steam for output path
+
    readStream.on('data', chunk => {
       if (enc) {
+         // if its for encryptions, encrypt data before write
          writeSream.write(encrypt(chunk));
-
-      }
-      else {
+      } else {
          writeSream.write(decrypt(chunk));
       }
 
    });
    readStream.on('end', () => {
-      console.log('ended');
-      if (!options.keep)
+      console.log(i)
+      if (!options.keep) // if user wants to keep old file dont delete, else do.
       fs.unlinkSync(i);
    })
 }
 
-if(!fs.existsSync(dir)) {
-   console.log('Error: ENOENT')
-   return;
+if(!fs.existsSync(dir)) { // throw error if file not found
+   throw new Error('Error: ENOENT');
 }
 
 if (command === 'encrypt') {
-   if (fs.statSync(dir).isDirectory()) {
+   if (fs.statSync(dir).isDirectory()) { //handle if the input os a directory
       const files = fs.readdirSync(dir);
       files.forEach(file => {
          readAndWrite(dir+file, dir+file+'.enc', true)
@@ -71,11 +77,12 @@ if (command === 'encrypt') {
 
 } else {
    if(fs.statSync(dir).isDirectory()) {
+
       const files = fs.readdirSync(dir).filter(file => {
          return /.enc$/.test(file);
-         //removes files which are not encrypted. (not with .enc ext)
+         //ignore files which are not encrypted. (not with .enc ext)
       });
-      console.log(files)
+      // console.log(files)
       files.forEach(file => {
          let outputFile = dir+file.slice(0, file.length-4)
          if (options.keep) {
